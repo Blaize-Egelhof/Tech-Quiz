1603fc8835b3feb9c03867e534d8e0df59c501cb
Blaize-Egelhof <>
2023-07-06 11:14:52 +0000

    feat! Add logic to start quiz button and quit quiz button

--- javascript.js
+++ javascript.js
@@ -2,15 +2,16 @@
 // create an array that holds first question,answer and options. 
 //My logic is IF answer === to 1 of the options then its correct , run a function
 //Variable which has an array assigned to it, the array has an object, said object has properties .
+
 let question1 = [
     {
-    num1: 1 ,
-    question: "What does CPU stand for?" ,
-    answer: "Central Processing Unit" ,
-        options: ["Central Processing Unit", "Central Peering Unit ", "Conjuncted Pile Unity " , "Centering Pivot Unit"] 
+        num1: 1,
+        question: "What does CPU stand for?",
+        answer: "Central Processing Unit",
+        options: ["Central Processing Unit", "Central Peering Unit ", "Conjuncted Pile Unity ", "Centering Pivot Unit"]
     }
-        
-]
+
+];
 
 let question2 = [
     {
@@ -31,3 +32,31 @@ let question3 = [
     }
 
 ];
+
+// Start Page Buttons
+
+let start_btn = document.querySelector('.start_btn_container > button');
+let feedback_btn = document.querySelector('.start_btn_container > button')[1];
+let info_box = document.getElementsByClassName('info_box')[0];
+let quit_btn = document.getElementsByClassName('quit')[0];
+console.log(quit_btn);
+
+
+//function to respond to when start quiz button is clicked
+//On click activate the class to reveal rules of quiz and disable class for front page
+
+start_btn.onclick = function showQuizRules() {
+    console.log('BUTTON IS CLICKED');
+    info_box.classList.add("activeInfo");  //show info box
+    console.log(info_box);
+};
+
+//function to respond to when user clicks Exit Quiz button on info box
+//On click  remove the activate class to hide info box.
+
+quit_btn.onclick = function hideQuizRules() {
+    console.log('BUTTON IS CLICKED');
+    info_box.classList.remove("activeInfo");
+};
+
+
