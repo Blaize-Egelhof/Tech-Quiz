95c1950453bf6f27bb4fbc63727e6ebbf856c5cd
Blaize-Egelhof <>
2023-07-06 09:19:35 +0000

    feat! create variables to hold questions and answers

---
+++ javascript.js
@@ -0,0 +1,33 @@
+//DISCLAIMER , I need to explain this code in detail so that I dont get confused !
+// create an array that holds first question,answer and options. 
+//My logic is IF answer === to 1 of the options then its correct , run a function
+//Variable which has an array assigned to it, the array has an object, said object has properties .
+let question1 = [
+    {
+    num1: 1 ,
+    question: "What does CPU stand for?" ,
+    answer: "Central Processing Unit" ,
+        options: ["Central Processing Unit", "Central Peering Unit ", "Conjuncted Pile Unity " , "Centering Pivot Unit"] 
+    }
+        
+]
+
+let question2 = [
+    {
+        num2: 2,
+        question: "What OS system is the fastest out of the below options?",
+        answer: "Linux",
+        options: ["Windows 10", "Windows 11", "Linux", "MacOS"]
+    }
+
+];
+
+let question3 = [
+    {
+        num3: 3,
+        question: "What OS system is the fastest out of the below options?",
+        answer: "Linux",
+        options: ["Windows 10", "Windows 11", "Linux", "MacOS"]
+    }
+
+];
